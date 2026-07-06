b244eeb3a1db70bf3e5e76809e1992416b4a042a
Ht <htcvhs971@gmail.com>
2026-07-06 12:25:52 +0200

    FIX: untracked

---
+++ src/utils/enums/EnumSalesPeriod.ts
@@ -0,0 +1,10 @@
+export enum EnumSalesPeriodType {
+  PROMOTION = 'promotion',
+  COMING_SOON = 'coming_soon',
+  EVENT = 'event',
+}
+
+export enum DiscountType {
+  PERCENT = 'PERCENT',
+  FIXED = 'FIXED',
+}
